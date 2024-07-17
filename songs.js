db9e5eb078719eb05ce8e7e6494fc2ce039ece1d
David <140751426+zeusswalle@users.noreply.github.com>
2024-07-16 22:11:13 -0300

    subir meu site

---
+++ songs.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        src: "/sounds/Apocalypse - Cigarettes After Sex.mp3",
+        name: "Apocalypse",
+    },
+    {
+        src: "/sounds/Love Grows (Where My Rosemary Goes) - Edison Lighthouse.mp3",
+        name: "Love Grows",
+    },
+    {
+        src: "/sounds/My Kind Of Woman - Mac DeMarco.mp3",
+        name: "My Kind Of Woman",
+    },
+    {
+        src: "/sounds/Lovers Rock.mp3",
+        name: "Lovers Rock - TV Girl"
+    },
+    {
+        src: "/sounds/Falling Behind - Laufey.mp3",
+        name: "Falling Behind",
+    },
+
+];
